d12e844d2db6182e2112858486bae3d11d799dc9
Franco Sardi Bottino <109819861+Franco-Sardi@users.noreply.github.com>
2023-04-04 00:05:51 -0300

    Update registro-producto.controller.js

--- controllers/registro-producto.controller.js
+++ controllers/registro-producto.controller.js
@@ -5,7 +5,7 @@ const formulario =  document.querySelector('[data-form]');
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const numeroAleatorio = Math.floor(Math.random() * 6)
-    const imagen  = `assets/producto-${numeroAleatorio}.png`;
+    const imagen  = `../assets/producto-${numeroAleatorio}.png`;
     const categoria = document.querySelector("[data-categoria]").value;
     const nombre = document.querySelector("[data-product]").value;
     const precio = document.querySelector("[data-precio]").value;
